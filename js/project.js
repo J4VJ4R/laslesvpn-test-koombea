bd4dfa42bdd0a90e68c0cfab5fabbde2888bf20d
Andmontc <oscarandresmontes@gmail.com>
2021-03-17 14:15:47 -0500

    Feature: Header section

---
+++ js/project.js
@@ -0,0 +1,19 @@
+const $menu = document.querySelector('.js-header-menu');
+const $head = document.querySelector('.js-header');
+const sticky = $head.offsetTop;
+
+window.onscroll = handleStickyScroll
+
+$menu.addEventListener('click', () => {
+  const header = $head.querySelector('.header__container');
+  header.classList.toggle('header__active');
+})
+
+function handleStickyScroll () {
+  if (window.pageYOffset > sticky) {
+    $head.classList.add('header__sticky');
+    return
+  }
+
+  $head.classList.remove('header__sticky');
+}
