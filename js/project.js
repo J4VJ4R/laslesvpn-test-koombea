2b7a089d273f3740aabf000395bd58bcc32e83f9
Andmontc <oscarandresmontes@gmail.com>
2021-03-22 20:49:39 -0500

    1- adding section testimonials1 Feature: customers and sponsors section

--- js/project.js
+++ js/project.js
@@ -1,25 +1,80 @@
 const $menu = document.querySelector('.js-header-menu');
 const $head = document.querySelector('.js-header');
+const $foot = document.querySelector('.js-footer-ontop-click');
 const sticky = $head.offsetTop;
+let footerOntopClick = $('.js-footer-ontop-click')
 
-window.onscroll = handleStickyScroll
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin: 35,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:true
+        },
+        1440:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+  });
 
-$menu.addEventListener('click', () => {
-  const header = $head.querySelector('.header__container');
-  header.classList.toggle('header__active');
-})
+  window.onscroll = handleStickyScroll
 
-function handleStickyScroll () {
-  if (window.pageYOffset > sticky) {
-    $head.classList.add('header__sticky');
-    return
+  $menu.addEventListener('click', () => {
+    const header = $head.querySelector('.header__container');
+    header.classList.toggle('header__active');
+  })
+
+  function handleStickyScroll () {
+    if (window.pageYOffset > sticky) {
+      $head.classList.add('header__sticky');
+      return
+    }
+
+    $head.classList.remove('header__sticky');
   }
-
-  $head.classList.remove('header__sticky');
-}
-//arrow that go to top
-$(document).ready(function (){
-  $(footerOntopClick).click(function (){
+  //arrow that go to top
+  $foot.addEventListener('click', () => {
     $('html, body').animate({scrollTop : 0},800);
   });
+});
+//slider testimonials1
+$(document).ready(function(){
+  let testimonials = $('.js-slides-item').length;
+  let imgPos = 1;
+  $('.js-slides-item').hide();
+  $('.js-slides-item:nth-child(2)').show();
+  //executing all functions
+  $('.js-slides__prev').click(prevSlider);
+  $('.js-slides-next').click(nextSlider);
+  //functions
+  function nextSlider(){
+    if(imgPos >= testimonials){
+      imgPos = 1;
+    } else {
+      imgPos++;
+    }
+    $('.js-slides-item').hide();
+    $('.js-slides-item:nth-child('+ imgPos +')').show();
+    // $('.js-slides-next span:nth-child('+ imgPos +')').css({'background-color': 'red'});
+  }
+  function prevSlider(){
+    if(imgPos <= 1){
+      imgPos = testimonials;
+    } else {
+      imgPos--;
+    }
+    console.log(imgPos);
+    $('.js-slides-item').hide();
+    $('.js-slides-item:nth-child('+ imgPos +')').show();
+    
+  }
 });
