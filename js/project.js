4a175ef6d36edf67736dc8ff8af817dc0675806d
Andmontc <oscarandresmontes@gmail.com>
2021-03-22 20:49:39 -0500

    Feature: customers and sponsors section

--- js/project.js
+++ js/project.js
@@ -1,25 +1,47 @@
 const $menu = document.querySelector('.js-header-menu');
 const $head = document.querySelector('.js-header');
+const $foot = document.querySelector('.js-footer-ontop-click');
 const sticky = $head.offsetTop;
 
-window.onscroll = handleStickyScroll
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin: 35,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:true
+        },
+        1440:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+  });
 
-$menu.addEventListener('click', () => {
-  const header = $head.querySelector('.header__container');
-  header.classList.toggle('header__active');
-})
+  window.onscroll = handleStickyScroll
 
-function handleStickyScroll () {
-  if (window.pageYOffset > sticky) {
-    $head.classList.add('header__sticky');
-    return
+  $menu.addEventListener('click', () => {
+    const header = $head.querySelector('.header__container');
+    header.classList.toggle('header__active');
+  })
+
+  function handleStickyScroll () {
+    if (window.pageYOffset > sticky) {
+      $head.classList.add('header__sticky');
+      return
+    }
+
+    $head.classList.remove('header__sticky');
   }
-
-  $head.classList.remove('header__sticky');
-}
-//arrow that go to top
-$(document).ready(function (){
-  $(footerOntopClick).click(function (){
+  //arrow that go to top
+  $foot.addEventListener('click', () => {
     $('html, body').animate({scrollTop : 0},800);
   });
-});
+})
